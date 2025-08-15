c2da090727371d5256217c472b7e1311cfca438d
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-08-15 15:48:26 +0700

    Membuat middleware route

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    try {
+        const pathname = request.nextUrl.pathname;
+        const token = request.cookies.get('jwt_token')?.value || '';
+
+        const isPrivate = pathname.startsWith('/user') || pathname.startsWith('/admin');
+
+        if (isPrivate && !token) {
+            return NextResponse.redirect(new URL('/?form=login', request.url))
+        }
+
+        const role = request.cookies.get('user_role')?.value || '';
+
+        if (token && !isPrivate) {
+            return NextResponse.redirect(new URL(`/${role}/dashboard`, request.url))
+        }
+
+        return NextResponse.next();
+    } catch (error) {
+        console.error('middleware error: ', error);
+        return NextResponse.redirect(new URL('/?form=login', request.url))
+    }
+}
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
