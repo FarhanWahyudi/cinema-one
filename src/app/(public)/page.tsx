d66af926bc396a8d13601e106f0f2207b83247d3
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-08-15 10:52:00 +0700

    Membuat tombol open auth sheet

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,36 +2,66 @@
 
 import { Button } from "@/components/ui/button"
 import { DotLottieReact } from "@lottiefiles/dotlottie-react"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { useState } from "react"
+import LoginForm from "./_components/login-form"
+import RegisterForm from "./_components/register-form"
+import { useSearchParams } from "next/navigation"
 
 export default function Homepage() {
-  return (
-    <div className="flex flex-col">
-        <div className="px-20 py-5 flex justify-between items-center bg-primary">
-            <h1 className="text-xl text-white font-bold">
-                Cinego
-            </h1>
-            <Button variant={'outline'}>Login</Button>
-        </div>
-        <div className="grid lg:grid-cols-2 gap-10 min-h-[85vh] items-center px-20">
-            <div className="col-span-1">
-                <div className="flex flex-col gap-4">
-                    <h1 className="text-lg font-bold">
-                        Book Your Movie Tickets Instantly
-                    </h1>
-                    <p className="text-sm font-semibold text-gray-500">
-                        CineGo is your one-stop platform for booking movie tickets across multiple theaters. Browse the latest movies, select your preferred seats, and enjoy a smooth, hassle-free booking experience. Whether it's a weekend blockbuster or a weekday show, CineGo has you covered.
-                    </p>
-                    <Button className="w-max">Get Started</Button>
+    const [openSheet, setOpenSheet] = useState(false);
+    const queryStrings = useSearchParams();
+    const form = queryStrings.get('form');
+        return (
+        <div className="flex flex-col">
+            <div className="px-20 py-5 flex justify-between items-center bg-primary">
+                <h1 className="text-xl text-white font-bold">
+                    Cinego
+                </h1>
+                <Button onClick={() => setOpenSheet(true)} variant={'outline'}>Login</Button>
+            </div>
+            <div className="grid lg:grid-cols-2 gap-10 min-h-[85vh] items-center px-20">
+                <div className="col-span-1">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="text-lg font-bold">
+                            Book Your Movie Tickets Instantly
+                        </h1>
+                        <p className="text-sm font-semibold text-gray-500">
+                            CineGo is your one-stop platform for booking movie tickets across multiple theaters. Browse the latest movies, select your preferred seats, and enjoy a smooth, hassle-free booking experience. Whether it's a weekend blockbuster or a weekday show, CineGo has you covered.
+                        </p>
+                        <Button className="w-max">Get Started</Button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <DotLottieReact
+                        src="https://lottie.host/5770f69e-4c8c-4605-abe5-c4e3608e9c9f/kSRTmm3nsL.lottie"    
+                        loop
+                        autoplay
+                    />
                 </div>
             </div>
-            <div className="col-span-1">
-                <DotLottieReact
-                    src="https://lottie.host/5770f69e-4c8c-4605-abe5-c4e3608e9c9f/kSRTmm3nsL.lottie"    
-                    loop
-                    autoplay
-                />
-            </div>
+            <Sheet
+                open={openSheet}
+                onOpenChange={(open) => setOpenSheet(open)}
+            >
+                <SheetContent className="min-w-[500px]">
+                    <SheetHeader>
+                        <SheetTitle></SheetTitle>
+                        <div className="flex flex-col items-center justify-center h-screen">
+                            {form === 'register' ? <RegisterForm /> : <LoginForm />}
+                        </div>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
         </div>
-    </div>
-  )
+    )
 }
