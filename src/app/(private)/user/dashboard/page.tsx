606a4118c98dd81956d7ae1fa602a22ec8334def
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-08-18 10:29:12 +0700

    Membuat tampilan dashboard

--- src/app/(private)/user/dashboard/page.tsx
+++ src/app/(private)/user/dashboard/page.tsx
@@ -1,17 +1,41 @@
-import { getLoggedInUser } from "@/actions/users"
-import UserInfo from "@/components/functional/user-info";
+"use client"
 
-export default async function UserDashboardPage() {
-    const UserDataResponse = await getLoggedInUser();
-    if (!UserDataResponse.success) {
-        return <h1>something went wrong</h1>
-    }
+import DashboardCard from "@/components/functional/dashboard-card";
+import PageTitle from "@/components/ui/page-title";
+import { useState } from "react";
 
-    const user = UserDataResponse.data
+export default function UserDashboardPage() {
+    const [dashboardData, setDashboardData] = useState({
+        totalBookings: 0,
+        totalMoviesWatched: 0,
+        totalTicketsBooked: 0,
+        totalAmountSpent: 0
+    })
     return (
         <div>
-            user dashboard
-            <UserInfo user={user} />
+            <PageTitle title="User Dashboard" />
+            <div className="grid grid-cols-4 gap-5 mt-5">
+                <DashboardCard
+                    title="Total Bookings"
+                    value={dashboardData.totalBookings}
+                    description="Total nummber of bookings made by the user"
+                />
+                <DashboardCard
+                    title="Total Watched"
+                    value={dashboardData.totalMoviesWatched}
+                    description="Total nummber of movie watched by user"
+                />
+                <DashboardCard
+                    title="Tickets Booked"
+                    value={dashboardData.totalTicketsBooked}
+                    description="Total nummber of tickets booked by the user"
+                />
+                <DashboardCard
+                    title="Amount Spent"
+                    value={dashboardData.totalAmountSpent}
+                    description="Total amount spent by the user on bookings"
+                />
+            </div>
         </div>
     )
 }
---
+++ src/components/functional/dashboard-card.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface DashboardCardProps {
+    title: string
+    value: number
+    description: string
+}
+
+export default function DashboardCard({ title, value, description }: DashboardCardProps) {
+    return (
+        <div className='bg-gray-100 p-5 flex flex-col gap-5 rounded border border-gray-500'>
+            <h1 className='text-sm font-bold uppercase'>{title}</h1>
+            <h1 className='text-6xl font-bold text-gray-700'>{value}</h1>
+            <p className='text-xs text-gray-500'>{description}</p>
+        </div>
+    )
+}
