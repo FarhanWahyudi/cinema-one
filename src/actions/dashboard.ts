e0663ce6a47c886bb15d07c2ad27c224b5daa5d3
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-08-18 13:03:17 +0700

    Membuat admin dashboard

--- src/actions/dashboard.ts
+++ src/actions/dashboard.ts
@@ -46,3 +46,72 @@ export const getUserDashboardData = async (userId: string) => {
         }
     }
 }
+
+export const getAdminDashboardData = async () => {
+    try {
+        const [moviesResponse, theatresResponse, showsResponse, bookingsResponse, usersResponse] = await Promise.all([
+            supabase.from('movies').select('id'),
+            supabase.from('theatres').select('id'),
+            supabase.from('shows').select('id'),
+            supabase.from('bookings').select('*'),
+            supabase.from('user_profiles').select('id'),
+        ])
+
+        if (
+            moviesResponse.error ||
+            theatresResponse.error ||
+            showsResponse.error ||
+            bookingsResponse.error ||
+            usersResponse.error 
+        ) {
+            throw new Error(
+                moviesResponse.error?.message ||
+                theatresResponse.error?.message ||
+                showsResponse.error?.message ||
+                bookingsResponse.error?.message ||
+                usersResponse.error?.message ||
+                'Error fetching data'
+
+            )
+        }
+
+        let responseData: any = {
+            totalMovies: moviesResponse.data?.length || 0,
+            totalThetres: theatresResponse.data?.length || 0,
+            totalShows: showsResponse.data?.length || 0,
+            totalBookings: bookingsResponse.data?.length || 0,
+            totalUsers: usersResponse.data?.length || 0,
+        }
+
+        let totalRevenue = 0
+        let totalTicketsBooked = 0
+        let cancelledTickets = 0
+
+        bookingsResponse.data.forEach((booking: any) => {
+            totalTicketsBooked += booking.total_tickets
+            if (booking.status === 'booked') {
+                totalRevenue += booking.total_amount
+            }
+
+            if (booking.status === 'cancelled') {
+                cancelledTickets += booking.total_tickets
+            }
+        })
+
+        responseData.totalRevenue = totalRevenue
+        responseData.totalTicketsBooked = totalTicketsBooked
+        responseData.cancelledTickets = cancelledTickets
+
+        return {
+            success: true,
+            message: 'admin dashboard data fetch successfully',
+            data: responseData
+        }
+    } catch (error: any) {
+        return {
+            success: false,
+            message: error.message,
+            data: null
+        }
+    }
+}
--- src/app/(private)/admin/dashboard/page.tsx
+++ src/app/(private)/admin/dashboard/page.tsx
@@ -1,16 +1,101 @@
-'use client'
+"use client"
 
-import { getLoggedInUser } from "@/actions/users";
-import UserInfo from "@/components/functional/user-info";
-import { IUserStore, useUsersStore } from "@/store/users-store";
+import { getAdminDashboardData, getUserDashboardData } from "@/actions/dashboard";
+import DashboardCard from "@/components/functional/dashboard-card";
+import Spinner from "@/components/functional/spinner";
+import PageTitle from "@/components/ui/page-title";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function AdminDashboardPage() {
-    const { user } = useUsersStore() as IUserStore;
+    const [dashboardData, setDashboardData] = useState({
+        totalMovies:  0,
+        totalThetres:  0,
+        totalShows:  0,
+        totalBookings:  0,
+        totalUsers:  0,
+        totalRevenue: 0,
+        totalTicketsBooked: 0,
+        cancelledTickets: 0,
+    })
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
 
+    const fetchDashboardData = async () => {
+        try {
+            setLoading(true)
+            const response: any = await getAdminDashboardData()
+            if (response.success) {
+                setDashboardData(response.data)
+            } else {
+                toast.error(response.message)
+            }
+        } catch (error: any) {
+            setError(error.message)
+            toast.error('failed to fetch dashboard data')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchDashboardData()
+    }, [])
+
+    if (loading) {
+        return <Spinner />
+    }
+
+    if (error) {
+        return <h1>{error}</h1>
+    }
     return (
         <div>
-            admin dashboard
-            {user && <UserInfo user={user} />}
+            <PageTitle title="Admin Dashboard" />
+            <div className="grid grid-cols-4 gap-5 mt-5">
+                <DashboardCard
+                    title="Total Movies"
+                    value={dashboardData.totalMovies}
+                    description="Total nummber of movies available in the system"
+                />
+                <DashboardCard
+                    title="Total Theatres"
+                    value={dashboardData.totalThetres}
+                    description="Total nummber of theatres available in the system"
+                />
+                <DashboardCard
+                    title="Total Shows"
+                    value={dashboardData.totalShows}
+                    description="Total nummber of shows available in the system"
+                />
+                <DashboardCard
+                    title="Total Bookings"
+                    value={dashboardData.totalBookings}
+                    description="Total nummber of bookings available in the system"
+                />
+                <DashboardCard
+                    title="Total Tickets Booked"
+                    value={dashboardData.totalTicketsBooked}
+                    description="Total nummber of tickets booked available in the system"
+                />
+                <DashboardCard
+                    title="Canclled Tickets"
+                    value={dashboardData.cancelledTickets}
+                    description="Total nummber of tickets that were cancelled"
+                />
+                <DashboardCard
+                    title="Total Users"
+                    value={dashboardData.totalUsers}
+                    description="Total nummber of user registered in the system"
+                />
+                <DashboardCard
+                    title="Total Revenue"
+                    value={dashboardData.totalRevenue}
+                    description="Total amount spent by users on bookings"
+                    isCurrency={true}
+                />
+                
+            </div>
         </div>
     )
 }
